39710970ef131d0bdf3a8d29dea72c3ae426766b
Luana Szreider <luanaszreider0@gmail.com>
2024-09-21 20:06:14 -0300

    método encontrarMaiorVenda

--- Bonus.java
+++ Bonus.java
@@ -1,12 +1,12 @@
 public class Bonus{
     
-    public static int validarDados(ArrayList<Departamento> listaDepartamentos) throws Exception{
+    public static int encontrarMaiorVenda(ArrayList<Departamento> listaDepartamentos) throws Exception{
+        //ver se a lista de departamentos ta vazia
         if (listaDepartamentos.isEmpty()){
-            throw new Exception("Código 1! Sem departamentos.");
+            throw new Exception("Código 1! Sem departamentos."); //codigo da exceção 
         }
-    }
 
-        boolean temFuncionarios = false;
+        boolean temFuncionarios = false; //vamos ver se tem algum funcionario já registrado nos departamentos
 
         for(Departamento departamento : listaDepartamentos){
             if(!departamento.getFuncionarios().isEmpty()){
@@ -16,7 +16,19 @@ public class Bonus{
         }
 
         if(!temFuncionarios){
-            throw new Exception("Código 1! Sem funcionários.")
+            throw new Exception("Código 1! Sem funcionários.") //se não tiver é código 1 tbm
         }
 
+         Departamento departamentoMaiorVenda = listaDepartamentos.get(0); //vou iniciar uma busca pelo departamento que mais vendeu, começando pelo primeiro índice
+
+         for (Departamento departamento : listaDepartamentos) {
+            if (departamento.getTotalVendas() > departamentoMaiorVenda.getTotalVendas()) {
+                departamentoMaiorVenda = departamento; //armazenei o departamento de maior venda aqui!
+            }
+        }
+        return departamentoMaiorVenda; 
+        
+    }
+
+        
 }
